e8679cfe0f5b9807cec94b264783f4b19ba1f324
alantmwong <95393427+alantmwong@users.noreply.github.com>
2023-01-19 12:55:26 +1300

    Completed test 1 exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, value) {
+    let index = array.indexOf(value);
+    array.splice(index,1);
+    return array
 };
 
 // Do not edit below this line
