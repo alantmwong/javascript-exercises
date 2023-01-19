b337661ae669ba556468af22e64b69a46013ab10
Alan <alantmwong1@gmail.com>
2023-01-19 21:15:10 +1300

    completed exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(minNumber, maxNumber) {
+    let array = [];
+    if (minNumber > maxNumber) {
+        let placeholder = minNumber;
+        minNumber = maxNumber;
+        maxNumber = placeholder;
+    } 
+    if (minNumber < 0 || maxNumber < 0) {
+        return "ERROR"
+    }
 
+    if (typeof minNumber != "number" || typeof maxNumber != "number") {
+        return "ERROR"
+    }
+
+    for (let i = minNumber; i < (maxNumber - minNumber + 2); i++) {
+        array[i] = i;
+    }
+    array = array.filter(Number);
+
+    let sumNumbers = array.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+    return sumNumbers
 };
 
 // Do not edit below this line
