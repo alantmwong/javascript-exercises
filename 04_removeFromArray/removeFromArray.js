d792dd889d8e9d0a8e46e894b268063211f11f00
Alan <alantmwong1@gmail.com>
2023-01-19 20:39:41 +1300

    Completed exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function(array, value) {
-    let index = array.indexOf(value);
-    array.splice(index,1);
+const removeFromArray = function(array, ...values) {
+    let index = 0;
+    for (let i = 1; i < (values.length + 1); i++) {
+        if (array.includes(values[i-1])) {
+            index = array.indexOf(values[i-1]);
+            array.splice(index,1);
+        }
+        
+    }
     return array
 };
 
