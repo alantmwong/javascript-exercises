23235341539a461e6e580438cb231d9744d74a35
alantmwong <95393427+alantmwong@users.noreply.github.com>
2023-01-28 15:18:53 +1300

    completed exercise 4 and 8

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,7 +1,17 @@
 const removeFromArray = function(array, ...values) {
+<<<<<<< HEAD
     for (let i = 1; i < arguments.length; i++){
         let index = array.indexOf(value);
         array.splice(index,1);
+=======
+    let index = 0;
+    for (let i = 1; i < (values.length + 1); i++) {
+        if (array.includes(values[i-1])) {
+            index = array.indexOf(values[i-1]);
+            array.splice(index,1);
+        }
+        
+>>>>>>> b337661ae669ba556468af22e64b69a46013ab10
     }
     return array
 };
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
 };
 
-const subtract = function() {
-	
+const subtract = function(firstNumber, secondNumber) {
+	return firstNumber - secondNumber;
 };
 
-const sum = function() {
-	
+const sum = function(arrayOfNumbers) {
+  return arrayOfNumbers.reduce(
+    (firstNumber,secondNumber) => add(firstNumber,secondNumber), 0);
 };
 
-const multiply = function() {
-
+const multiply = function(arrayOfNumbers) {
+  return arrayOfNumbers.reduce(
+    (firstNumber, secondNumber) => firstNumber * secondNumber, 1);
 };
 
-const power = function() {
-	
+const power = function(integer, exponent) {
+  array = [];
+  for (let i = 0; i < exponent; i++) {
+    array[i] = integer;
+  }
+  return multiply(array);
 };
 
-const factorial = function() {
-	
+const factorial = function(integer) {
+	let result = integer;
+  if (integer == 0 || integer == 1) {
+    return 1;
+  }
+  while (integer > 1) {
+    integer--;
+    result *= integer;
+  }
+  return result;
 };
 
 // Do not edit below this line
