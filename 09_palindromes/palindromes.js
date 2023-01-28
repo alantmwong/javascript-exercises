9ecafc92af7ae486b2e531a4cc53fb84e82ec00c
alantmwong <95393427+alantmwong@users.noreply.github.com>
2023-01-28 15:49:33 +1300

    part way through palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,22 @@
-const palindromes = function () {
+stringNoPunctuation = (string) => {
+    let array = string.split("");
+    const noPunctuation = array.filter(symbol => {
+        if (symbol != " " || symbol != "," || symbol != "!" || symbol != ".") {
+            return true;
+        }
+    })
+    return noPunctuation;
+};
 
+const palindromes = function (string) {
+    let noPunctuation = stringNoPunctuation(string);
+    let reverse = noPunctuation.reverse();
+    let joinArray = reverse.join("");
+    if (joinArray == string) {
+        return true;
+    } else {
+        return false;
+    }
 };
 
 // Do not edit below this line
