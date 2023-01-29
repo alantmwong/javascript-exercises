7d9d66a0cf54bcc310666da10f741f88fa54e0d0
alantmwong <95393427+alantmwong@users.noreply.github.com>
2023-01-29 13:24:22 +1300

    Finished palindromes and fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,10 @@
 const fibonacci = function(member) {
-    f
+    if (member < 0) return "OOPS";
+    array = [1,1];
+    for (i = 2; i < member; i++) {
+        array[i] = array[i-2] + array[i-1];
+    }
+    return array[member-1];
 };
 
 // Do not edit below this line
